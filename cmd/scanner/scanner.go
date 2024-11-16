ac3bd90581e18e7a6fc423d4bc3b25f5593d7a3f
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-11-17 00:53:04 +0200

    file scanner - replace 4 maps with one with &struct

--- cmd/scanner/scanner.go
+++ cmd/scanner/scanner.go
@@ -2,23 +2,40 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
+	"log/slog"
 	"os"
 	"strconv"
 	"strings"
 )
 
-// go run cmd/scanner/scanner.go  225.80s user 6.31s system 100% cpu 3:50.96 total
+// type NoHashingMap struct {
+// 	size int
+// }
+// // how to convert string to bucket index with minimal collisions? Options:
+// // 1. grab first letter and convert to unicode number, last letter to unicode number,
+// func (it NoHashingMap) makeBucketIndex(key string) int64 {
+// }
+//
+// func (it NoHashingMap) Set(key string, val int64) {
+// }
+//
+// func (it NoHashingMap) Get(key string) int64 {
+// }
+
+// go run cmd/scanner/scanner.go  104.55s user 4.98s system 100% cpu 1:48.68 total
 func main() {
 	file, _ := os.Open("./testfile")
 	defer file.Close()
 
 	// try pre-cache approx cities count
-	sums := make(map[string]int64, 1000)
-	minTemperatures := make(map[string]int64, 1000)
-	maxTemperatures := make(map[string]int64, 1000)
-	counts := make(map[string]int64, 1000)
+	type City struct {
+		Sum   int64
+		Min   int64
+		Max   int64
+		Count int64
+	}
+	cities := make(map[string]*City, 1000)
 
 	scanner := bufio.NewScanner(file)
 	// read till \n char
@@ -26,28 +43,31 @@ func main() {
 		// conv to string, possible replace with bytes?
 		line := scanner.Text()
 		// instead of cut, we can slice last 2 chars?
-		city, temp, found := strings.Cut(line, ";")
+		city, temperature, found := strings.Cut(line, ";")
 		if !found {
 			log.Fatal("Invalid line format " + line)
 		}
 		// better parseint?
 		// use unicode table to calculate digits from position of them in unicode?
-		intTemp, err := strconv.ParseInt(temp, 10, 8)
+		temp, err := strconv.ParseInt(temperature, 10, 8)
 		if !found {
 			log.Fatal("Cannot parse float", err)
 		}
-		// 4 times we hash again and again
-		counts[city]++
-		sums[city] += intTemp
-		minTemperatures[city] = min(minTemperatures[city], intTemp)
-		maxTemperatures[city] = max(minTemperatures[city], intTemp)
+		c, ok := cities[city]
+		if !ok {
+			cities[city] = &City{Count: 1, Sum: temp, Min: temp, Max: temp}
+			continue
+		}
+		c.Count += 1
+		c.Sum += temp
+		c.Min = min(c.Min, temp)
+		c.Max = min(c.Max, temp)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	for k, v := range sums {
-		fmt.Printf("City - %s, min: %d, max: %d, count: %d, sum: %d\n", k, minTemperatures[k], maxTemperatures[k], counts[k], v)
+	for _, v := range cities {
+		slog.Any("City", v)
 	}
-
 }
