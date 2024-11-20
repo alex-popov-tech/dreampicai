d7c03331910b0ea2652079ac7f038bf5deba4b51
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-11-20 10:00:01 +0200

    file scanner - seventh version mmap + scanner

--- cmd/scanner/scanner.go
+++ cmd/scanner/scanner.go
@@ -2,51 +2,94 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"os"
-	"strconv"
+	"syscall"
 	"time"
+
 	// "github.com/pkg/profile"
 )
 
-const semicolonByte = 59
+type MMap []byte
+func Map(path string) (MMap, error) {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
 
-var semicolonBytes = []byte{59}
-var linebreakByte = "\n"[0]
+	fi, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+	size := fi.Size()
 
-// o run cmd/scanner/scanner.go  54.51s user 3.14s system 99% cpu 57.932 total
+	data, err := syscall.Mmap(
+		int(f.Fd()),
+		0,
+		int(size),
+		syscall.PROT_READ,
+		syscall.MAP_SHARED,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return MMap(data), nil
+}
+
+func (m MMap) Unmap() error {
+	return syscall.Munmap(m)
+}
+
+type City struct {
+	Name  []byte
+	Sum   uint
+	Count uint
+	Min   uint
+	Max   uint
+}
+
+// go run cmd/scanner/scanner.go  51.41s user 3.09s system 98% cpu 55.426 total
 func main() {
 	// p := profile.Start(
-	// 	profile.CPUProfile,
-	// 	// profile.MemProfile,
+	// 	// profile.CPUProfile,
+	// 	profile.MemProfile,
 	// 	profile.ProfilePath("."),
 	// 	profile.NoShutdownHook,
 	// )
 	// defer p.Stop()
 	start := time.Now()
-	file, _ := os.Open("./testfile")
+	mmap, err := Map("./testfile")
+	// mmap, err := Map("./testfile_small")
+	if err != nil {
+		log.Fatal(err)
+	}
 	afterOpen := time.Now()
 	fmt.Println("Time spent opening file is seconds -", afterOpen.Sub(start).Seconds())
-	defer file.Close()
 
-	type City struct {
-		Sum   uint
-		Count uint
-		Min   uint
-		Max   uint
-	}
-	cities := make(map[string]*City, 1000)
+	bytesReader := bytes.NewReader(mmap)
+	scanner := bufio.NewScanner(bytesReader)
+	defer mmap.Unmap()
 
-	scanner := bufio.NewScanner(file)
+	cities := make(map[string]*City, 5000)
+
 	for scanner.Scan() {
-		lineBytes := scanner.Bytes()
-		cityBytes, temperatureBytes := separateBySemi(lineBytes)
+		line := scanner.Bytes()
 
-		temp, _ := strconv.ParseUint(string(temperatureBytes), 10, 8)
-		c := cities[string(cityBytes)]
+		semiIndex := bytes.IndexByte(line, ';')
+
+		city := line[:semiIndex]
+		temperature := line[semiIndex+1:]
+
+		temp := fastParseUint(string(temperature))
+		c := cities[string(city)]
+
 		if c == nil {
-			cities[string(cityBytes)] = &City{
+			cities[string(city)] = &City{
+				Name:  city,
 				Count: 1,
 				Sum:   uint(temp),
 				Min:   uint(temp),
@@ -63,16 +106,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for k, v := range cities {
-		fmt.Printf("%s, sum=%d, min=%d, max=%d, count=%d\n", k, v.Sum, v.Min, v.Max, v.Count)
+	for _, v := range cities {
+		fmt.Printf("%s, sum=%d, min=%d, max=%d, count=%d\n", v.Name, v.Sum, v.Min, v.Max, v.Count)
 	}
 }
 
-func separateBySemi(row []byte) (cityName []byte, temperature []byte) {
-	for i := len(row) - 1; i > 0; i-- {
-		if row[i] == semicolonByte {
-			return row[:i], row[i+1:]
-		}
+func fastParseUint(s string) uint64 {
+	if len(s) == 0 || len(s) > 2 {
+		return 0
 	}
-	panic("separateBySemi failed on " + string(row))
+
+	if len(s) == 1 {
+		ch := s[0]
+		if ch < '0' || ch > '9' {
+			return 0
+		}
+		return uint64(ch - '0')
+	}
+
+	// Two-digit parsing
+	if s[0] < '0' || s[0] > '7' || s[1] < '0' || s[1] > '9' {
+		return 0
+	}
+
+	return uint64((s[0]-'0')*10 + (s[1] - '0'))
 }
+
